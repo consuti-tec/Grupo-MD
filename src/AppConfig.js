e2a237f298fe5620ff0fbaa194f0ad168fc93973
consuti-tec <consuti@consuti.com.br>
2023-03-31 15:18:21 -0300

    Initial commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#FC6405",
+  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/estoril-esportes",
+  UrlPainel: "https://painel.escoladoesporte.com.br/",
+  TimeSplashScreen: 3000,
+};
